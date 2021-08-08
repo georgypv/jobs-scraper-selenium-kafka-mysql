712f3a57403707de4dcf25f48cd72cb376006c7e
georgypv <originalno93@yandex.ru>
2021-08-08 16:02:37 +0300

    rewrite oop

---
+++ myconfigs.py
@@ -0,0 +1,15 @@
+DRIVER_PATH = r'chromedriver/chromedriver.exe'
+HEADLESS = False
+
+
+MYSQL_USER  = 'python_user'
+MYSQL_PSWRD = '123'
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = '3306'
+MYSQL_TABLE_NAME = ''
+MYSQL_SCHEMA = ''
+
+
+KAFKA_HOST = 'localhost:9092'
+KAFKA_TOPIC = 'scraped_jobs_1'
+GROUP_ID = 'my_group'
